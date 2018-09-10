ceb5e11260fad41affa2a407b0561705f84b1b10
bazsa3333 <bazsa3333@gmail.com>
2018-09-10 17:14:59 +0200

    Initial Commit

---
+++ iOSTaskCornerWearables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTaskCornerWearables
+//
+//  Created by Bálint Németh on 2018. 09. 10..
+//  Copyright © 2018. Németh Bálint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
